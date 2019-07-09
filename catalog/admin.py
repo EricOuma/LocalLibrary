c941ea3411d7bb70b0cb6ea6e3ab606fa74f1f97
Eric Odhiambo <odhiamboeric0049@gmail.com>
2019-07-09 20:45:00 +0300

    setup administration

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,41 @@
 from django.contrib import admin
 
+from catalog.models import Author, Genre, Book, BookInstance
 # Register your models here.
+
+class AuthorBooksInline(admin.TabularInline):
+    model = Book
+
+# Define the admin class
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+    inlines = [AuthorBooksInline]
+
+
+admin.site.register(Genre)
+
+class BooksInstanceInline(admin.TabularInline):
+    model = BookInstance
+
+# Register the Admin classes for Book using the decorator
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre')
+    inlines = [BooksInstanceInline]
+
+# Register the Admin classes for BookInstance using the decorator
+@admin.register(BookInstance) 
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_display = ('book', 'status', 'due_back', 'id')
+    list_filter = ('status', 'due_back')
+
+    fieldsets = (
+        (None, {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back')
+        }),
+    )
