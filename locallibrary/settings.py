b0b64ae0210929aaa594cee7cc6c411ab745e431
Eric Odhiambo <odhiamboeric0049@gmail.com>
2019-08-20 15:55:58 +0300

    added new domain name to allowed hosts

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -48,7 +48,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddlewahttps://ericlibrary.herokuapp.com/re',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
